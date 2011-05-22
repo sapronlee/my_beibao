c4b48fd3a17982cfd35d071e39436943d21de98d
Chuck Lee <sapronlee@gmail.com>
2011-05-22 23:24:07 +0800

    升级版本好为0.0.3 beta, 1.添加设置备案号

--- config/initializers/default_settings.rb
+++ config/initializers/default_settings.rb
@@ -3,3 +3,7 @@
 Setting.defaults[:app_name] = "ChinaBeibao"
 # 网站名字
 Setting.defaults[:site_name] = "中国背包网"
+# 网站版本
+Setting.defaults[:version] = "0.0.3 beta"
+# 网站备案
+Setting.defaults[:icp] = "蜀ICP备10208266号"
